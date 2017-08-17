4b92d7761c433c934b742362f1c610c12eaa0ce1
mallory-evans <Mallory.Evans@nbcuni.com>
2017-08-17 10:41:54 -0400

    Update combine_network_data.R

--- Data Prep/combine_network_data.R
+++ Data Prep/combine_network_data.R
@@ -2,7 +2,7 @@
 library(plyr)
 library(dplyr)
 
-#set directory and read network data
+#set directory and read in minut-by-minute network data from Bravo, E!, Syfy, and USA
 directory <- ("C:/Users/206535096/Dropbox (NBCUniversal)/MSI_DSPractica/SoS_trends/data")
 
 brvo <- read.csv(file.path(directory, "CENT_18_49_V8_4_BRVO_non_agg.csv"))
@@ -10,7 +10,7 @@ e <- read.csv(file.path(directory, "CENT_18_49_V8_3_ENT_non_agg.csv"))
 syfy <- read.csv(file.path(directory, "CENT_18_49_V8_4_SYFY_non_agg.csv"))
 usa <- read.csv(file.path(directory, "CENT_18_49_V8_3_USA_non_agg.csv"))
 
-#aggregate data at show level
+#aggregate data at the episode level for each show and each stream (C3 and LS)
 brvo_showlevel <- plyr::ddply(subset(brvo, Source == "Nielsen" & LF_Season == 1), 
                               c("Network", "Broadcast_Year", "Cable_Qtr", "DOW", "Date", "Show_Name", 
                                 "Episode_num", "Season_num", "Start_Time", "End_Time", "Genre"), 
@@ -53,7 +53,7 @@ e_showlevel$Date <- as.Date(e_showlevel$Date)
 syfy_showlevel$Date <- as.Date(syfy_showlevel$Date)
 usa_showlevel$Date <- as.Date(usa_showlevel$Date, format = "%m/%d/%Y")
 
-#combine
+#combine all networks
 showlevel_data <- rbind(brvo_showlevel, e_showlevel, syfy_showlevel, usa_showlevel) %>% 
   arrange(Show_Name, Season_num, Episode_num)
 
@@ -69,12 +69,10 @@ showlevel_data$diff_C3_LS <- showlevel_data$C3_Impressions - showlevel_data$LS_I
 showlevel_data$higher_imps <- ifelse(showlevel_data$C3_Impressions > showlevel_data$LS_Impressions, "C3", "LS")
 showlevel_data$avg_C3_LS <- (showlevel_data$C3_Impressions + showlevel_data$LS_Impressions) / 2
 
-#create rebase (to 1st episode of season) and growth variables for C3 & LS variables
-showlevel_data <- showlevel_data %>% group_by(Show_Name, Season_num) %>% 
-  mutate(diff_C3_LS_rebase = diff_C3_LS - diff_C3_LS[1],
-         diff_C3_LS_growth = lag(diff_C3_LS, 1) / diff_C3_LS,
-         avg_C3_LS_rebase = avg_C3_LS - avg_C3_LS[1],
-         avg_C3_LS_growth = lag(avg_C3_LS, 1) / avg_C3_LS) %>% 
-  arrange(Show_Name, Season_num, Episode_num)
+#create rebase (to 1st episode of season) and growth for C3 & LS transformed variables
+showlevel_data$diff_C3_LS_rebase <- showlevel_data$C3_Rebase - showlevel_data$LS_Rebase
+showlevel_data$diff_C3_LS_growth <- showlevel_data$C3_Growth - showlevel_data$LS_Growth
+showlevel_data$avg_C3_LS_rebase <- (showlevel_data$C3_Rebase + showlevel_data$LS_Rebase) / 2
+showlevel_data$avg_C3_LS_growth <- (showlevel_data$C3_Growth + showlevel_data$LS_Growth) / 2
 
 write.csv(showlevel_data, "CENT_showlevel.csv")
